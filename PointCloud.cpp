dc37acdc7a9d8e3dedc9caa9befe52ac3db53627
FX <fx.thomas@gmail.com>
2011-04-25 23:58:54 +0200

    Added additional models

---
+++ PointCloud.cpp
@@ -0,0 +1,13 @@
+/**
+ * PointCloud C++ Source code (PointCloud.cpp)
+ * Created: Mon 25 Apr 2011 10:18:04 PM CEST
+ *
+ * This C source code was developped by François-Xavier Thomas.
+ * You are free to copy, adapt or modify it.
+ * If you do so, however, leave my name somewhere in the credits, I'd appreciate it ;)
+ * 
+ * @author François-Xavier Thomas <fx.thomas@gmail.com>
+ * @version 1.0
+ */
+
+#include "PointCloud.hpp"
